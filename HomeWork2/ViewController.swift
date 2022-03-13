50014d2d31377f9958cf8a315b6f82839b10ae6e
Vlad Kulakovsky <vayp97@gmail.com>
2022-03-13 14:02:59 +0300

    Initial Commit

---
+++ HomeWork2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork2
+//
+//  Created by Vlad Kulakovsky  on 13.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
