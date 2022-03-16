f93942becd7c8ce0c129f73ad8f2663ca92a1acf
Vlad Kulakovsky <vayp97@gmail.com>
2022-03-16 22:53:44 +0300

    version 2

--- HomeWork2/ViewController.swift
+++ HomeWork2/ViewController.swift
@@ -32,62 +32,58 @@ class ViewController: UIViewController {
         mainView.backgroundColor = UIColor.black
         mainView.layer.cornerRadius = 10
         // MARK: Start load parameters for valueTextRed, Green, Blue
-        valueTextRed.text = "0"
+        //valueTextRed.text = "0"
         valueTextRed.textColor = .white
-        valueTextBlue.text = "0"
+       // valueTextBlue.text = "0"
         valueTextBlue.textColor = .white
-        valueTextGreen.text = "0"
+        //valueTextGreen.text = "0"
         valueTextGreen.textColor = .white
+        
+        valueTextRed.text = String(Int(sliderRed.value))
+        
+        valueTextBlue.text = String(Int(sliderBlue.value))
+        
+        valueTextBlue.text = String(Int(sliderBlue.value))
+        
+        
         // MARK: Start load parameters for red slider
         sliderRed.value = 0
         sliderRed.minimumValue = 0
         sliderRed.maximumValue = 255
         sliderRed.tintColor = UIColor.red
-        valueTextRed.text = String(Int(sliderRed.value))
+       
         // MARK: Start load parameters for green slider
         sliderGreen.value = 0
         sliderGreen.minimumValue = 0
         sliderGreen.maximumValue = 255
         sliderGreen.tintColor = UIColor.green
-        valueTextBlue.text = String(Int(sliderBlue.value))
         // MARK: Start load parameters for blue slider
         sliderBlue.value = 0
         sliderBlue.minimumValue = 0
         sliderBlue.maximumValue = 255
         sliderBlue.tintColor = UIColor.blue
-        valueTextBlue.text = String(Int(sliderBlue.value))
+     
         // MARK: Start load parameters for switchers
         }
     
     
     @IBAction func RedSliderTextAction(_ sender: Any) {
         valueTextRed.text = String(Int(sliderRed.value))
-    }
-    
-    @IBAction func GreenSliderTextAction(_ sender: Any) {
         valueTextGreen.text = String(Int(sliderGreen.value))
-    }
-    
-    @IBAction func BlueSliderTextAction(_ sender: Any) {
         valueTextBlue.text = String(Int(sliderBlue.value))
     }
     
   // Change view color logic
     @IBAction func RedSliderChangeViewColor(_ sender: Any) {
         
-        mainView.backgroundColor = UIColor(red: CGFloat(sliderRed.value)/255, green: CGFloat(sliderGreen.value)/255, blue: CGFloat(sliderBlue.value)/255, alpha: 1)
+        mainView.backgroundColor = UIColor(red: CGFloat(sliderRed.value)/255,
+                                        green: CGFloat(sliderGreen.value)/255,
+                                        blue: CGFloat(sliderBlue.value)/255,
+                                        alpha: 1)
 }
     
     
-    @IBAction func GreenSliderChangeViewColor(_ sender: Any) {
 
-        mainView.backgroundColor = UIColor(red: CGFloat(sliderRed.value)/255, green: CGFloat(sliderGreen.value)/255, blue: CGFloat(sliderBlue.value)/255, alpha: 1)
-    }
-   
-    @IBAction func BlueSliderChangeViewColor(_ sender: Any) {
-
-        mainView.backgroundColor = UIColor(red: CGFloat(sliderRed.value)/255, green: CGFloat(sliderGreen.value)/255, blue: CGFloat(sliderBlue.value)/255, alpha: 1)
-    }
 
   // Switchers hide sliders by color
     @IBAction func HideRedSlider(_ sender: Any) {
@@ -95,10 +91,10 @@ class ViewController: UIViewController {
         valueTextRed.isHidden = !switcherRed.isOn
         if !switcherRed.isOn {
     
-            mainView.backgroundColor = UIColor(red: 0/255, green: CGFloat(sliderGreen.value)/255, blue: CGFloat(sliderBlue.value)/255, alpha: 1)
-        } else if switcherRed.isOn {
-            valueTextRed.text = String(Int(sliderRed.value))
-            mainView.backgroundColor = UIColor(red: CGFloat(sliderRed.value)/255, green: CGFloat(sliderGreen.value)/255, blue: CGFloat(sliderBlue.value)/255, alpha: 1)
+            mainView.backgroundColor = UIColor(red: CGFloat(sliderRed.value)/255,
+                                            green: CGFloat(sliderGreen.value)/255,
+                                            blue: CGFloat(sliderBlue.value)/255,
+                                            alpha: 1)
         }
     }
     
@@ -108,10 +104,10 @@ class ViewController: UIViewController {
         valueTextGreen.isHidden = !switcherGreen.isOn
         if !switcherGreen.isOn {
 
-            mainView.backgroundColor = UIColor(red: CGFloat(sliderRed.value)/255, green: 0/255, blue: CGFloat(sliderBlue.value)/255, alpha: 1)
-        } else if switcherGreen.isOn {
-            valueTextGreen.text = String(Int(sliderGreen.value))
-            mainView.backgroundColor = UIColor(red: CGFloat(sliderRed.value)/255, green: CGFloat(sliderGreen.value)/255, blue: CGFloat(sliderBlue.value)/255, alpha: 1)
+            mainView.backgroundColor = UIColor(red: CGFloat(sliderRed.value)/255,
+                                               green: CGFloat(sliderGreen.value)/255,
+                                            blue: CGFloat(sliderBlue.value)/255,
+                                            alpha: 1)
         }
     }
     
@@ -120,10 +116,10 @@ class ViewController: UIViewController {
         sliderBlue.isHidden = !switcherBlue.isOn
         valueTextBlue.isHidden = !switcherBlue.isOn
          if !switcherBlue.isOn {
-             mainView.backgroundColor = UIColor(red: CGFloat(sliderRed.value)/255, green: CGFloat(sliderGreen.value)/255, blue: 0/255, alpha: 1)
-         } else if switcherBlue.isOn {
-            valueTextBlue.text = String(Int(sliderBlue.value))
-            mainView.backgroundColor = UIColor(red: CGFloat(sliderRed.value)/255, green: CGFloat(sliderGreen.value)/255, blue: CGFloat(sliderBlue.value)/255, alpha: 1)
+             mainView.backgroundColor = UIColor(red: CGFloat(sliderRed.value)/255,
+                                            green: CGFloat(sliderGreen.value)/255,
+                                                blue: CGFloat(sliderBlue.value)/255,
+                                            alpha: 1)
         }
     }
     
